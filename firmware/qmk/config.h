2fe7cbaad943a0444f7e4251eece2c932cc93d7a
Alexandre Plateau <lexplt.dev@gmail.com>
2023-08-23 20:35:43 +0200

    updating qmk firmware since vial updated

--- firmware/qmk/config.h
+++ firmware/qmk/config.h
@@ -15,42 +15,12 @@
  */
 #pragma once
 
-#include "config_common.h"
-
 // OLED driver
 #ifdef OLED_DRIVER_ENABLE
   #define OLED_DISPLAY_128X32
   #define OLED_TIMEOUT 30000
 #endif
 
-// Liatris led configuration
-#undef RGB_DI_PIN
-#define RGB_DI_PIN 25
-#undef RGBLED_NUM
-#define RGBLED_NUM 2
-#undef RGBLED_SPLIT
-#define RGBLED_SPLIT {1, 1}
-
-// Use power indicator as caps lock indicator
-#define LED_CAPS_LOCK_PIN 24
-#define LED_PIN_ON_STATE 0
-
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-
-/* Serial settings */
-#define USE_SERIAL
-/* serial.c configuration for split keyboard */
-#define SOFT_SERIAL_PIN D2
-
-/* Config matrix */
-#define DIODE_DIRECTION COL2ROW
-#define MATRIX_ROW_PINS { D4, C6, D7, E6 }
-#define MATRIX_COL_PINS { B1, F7, F6, F5, F4 }
-
-/* Encoders config */
-#define ENCODERS_PAD_A { B4 }
-#define ENCODERS_PAD_B { B5 }
-#define ENCODERS_PAD_A_RIGHT { B5 }
-#define ENCODERS_PAD_B_RIGHT { B4 }
 
--- firmware/qmk/rules.mk
+++ firmware/qmk/rules.mk
@@ -13,14 +13,9 @@ BOOTLOADER = caterina
 CONVERT_TO = promicro_rp2040
 ENCODER_ENABLE = yes
 
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-UNICODE_ENABLE = yes
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
-SPLIT_KEYBOARD = yes
 RGBLIGHT_ENABLE = yes       # Enable underlight
 WS2812_DRIVER = vendor      # Use the RP2040's PIO interface
 

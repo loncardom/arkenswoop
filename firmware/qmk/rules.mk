407f1b139d6eccc70f322ad4f86ca282d5246eda
Dominik Loncar <domba96@gmail.com>
2024-12-10 20:02:27 -0500

    fixes for leonardo

--- firmware/qmk/rules.mk
+++ firmware/qmk/rules.mk
@@ -10,7 +10,7 @@ MCU = atmega32u4
 BOOTLOADER = caterina
 
 # Compile for the Liatris
-CONVERT_TO = promicro_rp2040
+# CONVERT_TO = promicro_rp2040
 ENCODER_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 
@@ -18,5 +18,5 @@ CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 RGBLIGHT_ENABLE = yes       # Enable underlight
-WS2812_DRIVER = vendor      # Use the RP2040's PIO interface
+# WS2812_DRIVER = vendor      # Use the RP2040's PIO interface
 
